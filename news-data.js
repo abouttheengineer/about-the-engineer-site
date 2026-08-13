e1564909dacf206b7d305361ba5714c86a1aeda2
lekam <hurikake.kome0066@gmail.com>
2026-08-13 23:06:47 +0900

    Add Vue Fes Japan 2026 sponsorship image to news article

--- news-data.js
+++ news-data.js
@@ -7,6 +7,6 @@
  * the image column only renders when an image is given.
  */
 window.NEWS = [
-  { date: '2026.08.13', title: '合同会社About The Engineerは、「Vue Fes Japan 2026」にブロンズスポンサーとして協賛決定！', desc: '2026年10月24日（土）に東京・大手町プレイス ホール＆カンファレンスにて開催される「Vue Fes Japan 2026」に、スポンサーとして協賛が決定しました。' },
+  { date: '2026.08.13', title: '合同会社About The Engineerは、「Vue Fes Japan 2026」にブロンズスポンサーとして協賛決定！', desc: '2026年10月24日（土）に東京・大手町プレイス ホール＆カンファレンスにて開催される「Vue Fes Japan 2026」に、スポンサーとして協賛が決定しました。', image: 'article/01.png', fit: 'contain' },
   { date: '2026.07.31', title: '車いすバスケットボールチーム「SAGAMIFORCE」のスポンサーになりました。', image: 'sponsor-sagamiforce-logo.jpg', fit: 'contain' },
 ];
